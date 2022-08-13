5a2afb9d4fad66b1b8cd449a76fcc9ed641d95fc
Cesar Augusto <augustocsar22@gmail.com>
2022-08-12 23:06:01 -0300

    aula de rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('user/{user}', function(\App\Models\User $user) {
+    dd($user);
+    return $user;
 });
+
+Route::prefix('usuarios')->group(function() {
+    Route::get('', function() {
+        return 'usuarios';
+    })->name('usuarios');
+    Route::get('/{id}', function() {
+        return 'Mostrar detalhes';
+    })->name('usuarios_show');
+    Route::get('/{id}/tags', function() {
+        return 'Tags do usuários';
+    })->name('usuarios_tags');
+});
+
+Route::get('/empresa/{string?}', function ($string = null) {
+    return $string;
+    // return view('welcome');
+})->name('empresa');
